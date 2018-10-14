2f3ea1ed6bd63643a177c25d01d1baaa56e5378b
jz <jz@pdx.ltd>
2018-10-14 13:00:06 +0800

    added http proxy support

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# PDX chainmux, an HTTP-CONNECT based TCP multiplexer
+# PDX chainmux, an HTTP-CONNECT based HTTP & TCP proxy 
 
 This service enables a blockchain node to have all of its 
 TCP endpoints behind this single "external" facing HTTP 
 endpoint, so that firewall traversal is less painful.
 
 When a node belongs to multiple blockchain instances, only
-this external facin HTTP endpoint needs to be exposed, not 
+this external facing HTTP endpoint needs to be exposed, not 
 the detailed endpoints of each chain.
 
 The whitelist approach effectively protects a node from 
 inadvertently exposing unwanted services. 
 
 Further, via redirect, a node's internal network and
-endpoint details can be hidden from outside world, which
+endpoint details can be hidden from the outside world, which
 ensures flexibility on the node-side network implementation.
 
 Any questions or suggestions, please contact info@pdx.ltd.
--- chainmux.go
+++ chainmux.go
@@ -12,6 +12,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/url"
 	"time"
 	"os"
 	"bufio"
@@ -19,7 +20,7 @@ import (
 	"fmt"
 )
 
-func redirectTo(conf string, asked string) string {
+func rewriteTo(conf string, asked string) string {
 
 	file, err := os.OpenFile(conf, os.O_RDONLY, os.ModeExclusive)
 	if err != nil {
@@ -46,9 +47,9 @@ func redirectTo(conf string, asked string) string {
 
 func handleTunneling(conf string, w http.ResponseWriter, r *http.Request) {
 
-	dst := redirectTo(conf, r.RequestURI)
+	dst := rewriteTo(conf, "conn://" + r.RequestURI)
 
-	//log.Println("requested ", r.RequestURI, ", redirected to:", dst)
+	log.Println("CONN: requested ", r.RequestURI, ", redirected to:", dst)
 
 	if dst == "" {
 		http.Error(w, r.RequestURI + " is not allowed", http.StatusServiceUnavailable)
@@ -80,15 +81,68 @@ func transfer(dst io.WriteCloser, src io.ReadCloser) {
 	io.Copy(dst, src)
 }
 
+func handleHTTP(conf string, w http.ResponseWriter, r *http.Request) {
+
+	url, err := url.Parse(r.RequestURI)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	asked := "http://" + url.Host
+	if url.Port() != "" {
+		asked += ":" + url.Port()
+	} else {
+		asked += ":80"
+	}
+
+	dst := rewriteTo(conf, asked) //host:port
+	if dst == "" {
+		http.Error(w, r.RequestURI + " is not allowed", http.StatusServiceUnavailable)
+		return
+	}
+
+	asked = r.RequestURI
+
+	r.RequestURI = "http://" + dst + url.RequestURI()
+
+	log.Println("HTTP: requested ", asked, ", redirected to:", r.RequestURI)
+
+	resp, err := http.DefaultTransport.RoundTrip(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+
+	defer resp.Body.Close()
+	copyHeader(w.Header(), resp.Header)
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
 func main() {
 
 	flag.Usage = func() {
 		fmt.Println("")
-		fmt.Println("PDX chainmux, an HTTP-CONNECT based, whitelisted redirecting TCP multiplexer")
+		fmt.Println("PDX chainmux, an HTTP-CONNECT based, whitelisted rewritable HTTP & TCP proxy")
 		fmt.Println("")
-		fmt.Println("-conf		The configuration file. Or via PDX_CHAINMUX_CONF_FILE env")
+		fmt.Println("-conf		The configuration file. Or via PDX_CHAINMUX_CONF_FILE environment variable")
 		fmt.Println("		One line for each whitelist item, first match is selected.")
-		fmt.Println("		Config format: requested-URI \\t redirected-URI \\n")
+		fmt.Println("		Config format: proto://asked_host:asked_port \\t target_host:target_port\\n")
+		fmt.Println("		Here, proto is conn for HTTP-CONNECT based tunneling, http for  http proxy")
+		fmt.Println("		For example,")
+		fmt.Println("			conn://chain-x:30303 localhost:30308")
+		fmt.Println("			http://pdx.ltd:80 localhost:80")
+		fmt.Println("")
 		fmt.Println("-port		The [host]:port chainmux listens on")
 		fmt.Println("")
                 fmt.Println("Please visit https://github.com/PDXbaap/chainmux to get the latest version.")
@@ -108,10 +162,15 @@ func main() {
 	}
 
 	server := &http.Server{
+		//ReadTimeout:  10 * time.Second,
+		//WriteTimeout: 10 * time.Second,
+		//IdleTimeout:  10 * time.Second,
 		Addr: port,
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if r.Method == http.MethodConnect {
 				handleTunneling(conf, w, r)
+			} else {
+				handleHTTP(conf, w, r)
 			}
 		}),}
 
